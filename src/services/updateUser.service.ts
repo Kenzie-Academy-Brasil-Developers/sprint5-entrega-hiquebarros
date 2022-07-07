41d0f39c443f2e359c7e586480f6c19e63a72823
Henrique Barros <henrique1_barros@hotmail.com>
2022-07-07 15:02:53 -0300

    fix: update route

--- src/services/updateUser.service.ts
+++ src/services/updateUser.service.ts
@@ -2,9 +2,10 @@ import { User } from "../entities/user.entity"
 import AppDataSource from "../data-source"
 import { IUpdateRequest } from "../interfaces/index"
 import { AppError } from "../errors/AppError"
+import { hash } from "bcryptjs"
 
 const updateUserService = async (userId: string, reqBody: IUpdateRequest): Promise<void> => {
-    
+
     const userRepository = AppDataSource.getRepository(User)
     const user = await userRepository.findOneBy({ id: userId })
 
@@ -12,12 +13,18 @@ const updateUserService = async (userId: string, reqBody: IUpdateRequest): Promi
         throw new AppError("User not found", 404)
     }
 
+    if (reqBody.password) {
+        const hashedPassword = await hash(reqBody.password, 10)
+        reqBody.password = hashedPassword
+    }
+    
     await AppDataSource
         .createQueryBuilder()
         .update(User)
         .set(reqBody)
         .where("id = :id", { id: userId })
         .execute()
+    
 }
 
 export default updateUserService
